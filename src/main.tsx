7c11fe2dedd93112ec4ca3fff0f5759346d75535
Madhu Singarapu <108062936+MS1204@users.noreply.github.com>
2025-11-25 19:55:11 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/we-stories">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
