34fa2e2b04474526c3a9504af8d88f8c1a98b64b
Madhu Singarapu <108062936+MS1204@users.noreply.github.com>
2025-11-26 01:31:49 +0530

    Switch from BrowserRouter to HashRouter
    Replaced BrowserRouter with HashRouter for routing.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 
